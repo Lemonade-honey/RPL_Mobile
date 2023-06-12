e045521c72c2737b3bfea82b08fc236564f1c37b
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2023-06-12 20:41:58 +0700

    Menambahkan Hook Fetch API

---
+++ hook/useFetch.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useFetch = ({endpoint}) => {
+
+    const [data, setData] = useState([]);
+    // default loading
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(false)
+
+
+    const option = {
+        method: 'GET',
+        url: `https://rplcoffe.000webhostapp.com/menu`,
+    }
+
+    // fetch data
+    const fetchData = async () => {
+        // loading screen saat mulai
+        setIsLoading(true);
+
+        try {
+            const response = await axios.request(option);
+            setData(response.data.data);
+        } catch (error) {
+            setError(error)
+            alert('Ada Error dalam API');
+        } finally{
+            // jika ada error atau data bisa diambil
+            setIsLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+
+    // fetch ulang, karena kadang data saat req lagi tidak termulai 
+    const reFetch = () => {
+        setIsLoading(true);
+        fetchData();
+    }
+
+    return {data, isLoading, error, reFetch}
+}
+
+export default useFetch
