bd47407ecc294d20a141d7d7c74a7617c4e5ccf7
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2023-06-12 20:42:25 +0700

    Menghubungkan API dengan UI

--- components/card/MenuCard.jsx
+++ components/card/MenuCard.jsx
@@ -2,20 +2,31 @@ import React from "react";
 import { View, Text, TouchableOpacity, Image, StyleSheet } from "react-native";
 
 
-const MenuCard = () => {
-    return(
-        <TouchableOpacity style={styles.container}>
-            <Image resizeMode="center"/>
-            <Text>menu</Text>
-        </TouchableOpacity>
-    )
+const MenuCard = ({target, type, nama, img, harga}) => {
+    if(target == type){
+        return(
+            <TouchableOpacity style={styles.container}>
+                <Image source={{uri: img}} style={{flex: 1,width: null, height: null, borderTopRightRadius: 10, borderTopLeftRadius: 10}} resizeMode="center"/>
+                <View style={styles.subContainer}>
+                    <Text>{nama}</Text>
+                    <Text>Rp. {harga}</Text>
+                </View>
+            </TouchableOpacity>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
     container: {
         height: 130,
         width: 150,
-        marginBottom: 30
+        marginBottom: 30,
+        borderWidth: 1,
+        borderColor: '#8a8a8a',
+        borderRadius: 10,
+    },
+    subContainer: {
+        paddingLeft: 5
     }
 })
 
--- components/home/menu/Menu.jsx
+++ components/home/menu/Menu.jsx
@@ -1,16 +1,46 @@
 import React from "react";
-import { View, Text, TouchableOpacity, Image } from "react-native";
+import { View, Text, ActivityIndicator } from "react-native";
 
 import styles from "./menu.style";
 
 import MenuCard from "../../card/MenuCard";
+import useFetch from "../../../hook/useFetch";
+import { COLORS } from "../../../constant";
 
-const Menu = ({ handlePress }) => {
+const Menu = () => {
+
+    // fetch data
+    const {data, isLoading, error} = useFetch('menu');
+
+
     return(
         <View style={styles.container}>
+            {/* Makanan Container */}
             <Text style={styles.containerTitle}>Makanan</Text>
             <View style={styles.subContainer}>
-                <MenuCard />
+                {isLoading ? (
+                    <ActivityIndicator size="large" color={COLORS.primary} />
+                ) : error ? (
+                    {/* <Text>Something went wrong</Text> */}
+                ) : (
+                    data?.map((item) => (
+                        <MenuCard key={item.id} target={'makanan'} type={item.type} nama={item.nama} harga={item.harga} img={`https://rplcoffe.000webhostapp.com/img/${item.img}`}/>
+                    ))
+                )}
+            </View>
+
+            {/* Minuman Container */}
+            <Text style={styles.containerTitle}>Minuman</Text>
+            <View style={styles.subContainer}>
+                {isLoading ? (
+                    <ActivityIndicator size="large" color={COLORS.primary} />
+                ) : error ? (
+                    {/* <Text>Something went wrong</Text> */}
+                ) : (
+                    data?.map((item) => (
+                        <MenuCard key={item.id} target={'minuman'} type={item.type} nama={item.nama} harga={item.harga} img={`https://rplcoffe.000webhostapp.com/img/${item.img}`}/>
+                    ))
+                )}
             </View>
         </View>
     );
