4b4aefb33259a4154357963d19861920570a0cd8
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2023-06-13 10:25:38 +0700

    Detail Menu berhasil diimplementasikan, tinggal desain UI

--- app/menu-detail/[id].js
+++ app/menu-detail/[id].js
@@ -1,11 +1,33 @@
-import { useRouter } from "expo-router";
-import { View, Text } from "react-native";
+import { Stack, useRouter, useSearchParams } from "expo-router";
+import { View, Text, SafeAreaView, ScrollView, ActivityIndicator } from "react-native";
+
+import useFetch from "../../hook/useFetch";
+import { COLORS } from "../../constant";
 
 const MenuDetail = () => {
     const router = useRouter();
+    const params = useSearchParams();
+
+    const {data, isLoading, error, reFetch} = useFetch("menu", params.id);
 
     return (
-        <Text>ANJAY</Text>
+        <SafeAreaView>
+            <Stack.Screen 
+                options={{
+                    headerTitle: "Menu Details"
+                }}
+            />
+            <ScrollView>
+                {isLoading ? (
+                    <ActivityIndicator size={"large"} color={COLORS.primary} />
+                ) : error ? (
+                    <Text>Something went wrong</Text>
+                ) : (
+                    <Text>Nama : {data.nama}</Text>
+                    // buat card
+                )}
+            </ScrollView>
+        </SafeAreaView>
     );
 }
 
